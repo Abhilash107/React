460c05e8bceafdb7e9a0d2b7e648299002b1f8d3
Abhilash107 <abhilash00003108884@gmail.com>
2024-08-03 20:30:33 +0530

    jsx

--- vite-React/src/App.jsx
+++ vite-React/src/App.jsx
@@ -1,11 +1,17 @@
 import Test from "./test.jsx"
 
 function App() {
+
+  const username = "Abhilash107"
+
   return (
     <><Test/>
-   <h1>Hello</h1></>
+   <h1>Hello, {username}</h1>
+   </>
     // error, export only 1 element
    //* Hence use div or <> </>(fragment)
+   //* we write evaluated expressions inside return
+   
   )
 }
 
--- vite-React/src/main.jsx
+++ vite-React/src/main.jsx
@@ -2,7 +2,48 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App | React</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//       href: 'https://google.com',
+//       target: "blank"
+//   },
+//   children: "Click me to visit google"
+// }
+
+const anotherElement = (
+  <a href="https:/google.com" target='_blank'>Google</a>
+)
+
+const anotherUser = "I am nobody"
+
+const reactElement = React.createElement(
+  'a',
+  {href: "https://google.com", target: '_blank'},
+  "Click me to visit google",
+  anotherUser
+)
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  //<MyApp />  //works
+  //MyApp()    // works
+
+  //ReactElement // Nope, due to incorrect syntax
+
+  //anotherElement    // works
+  reactElement        // works
+
+  //<App/>
+
+
 )
