f5dadf9c4a4751b48f9c8a5448aa9a58d6597634
Abhilash107 <abhilash00003108884@gmail.com>
2024-09-12 00:03:12 +0530

    components

---
+++ miniContext/src/components/Login.jsx
@@ -0,0 +1,35 @@
+import React,{useState, useContext} from "react";
+import UserContext from "../context/UserContext"
+
+function Login(){
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    // this setUser is used in props in UserContextProvider file
+    const {setUser} = useContext(UserContext)
+
+    //* Sending the data
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        setUser({username, password})
+    }
+    return (
+        <div>
+            <h2>Login</h2>
+            <input type="text" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="username" />
+            {"  "}
+
+            <input type="text" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="password" />
+            <button onClick={handleSubmit}>submit</button>
+        </div>
+    )
+}
+
+export default Login
+
