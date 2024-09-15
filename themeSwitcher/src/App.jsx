567ac148835c764076cd36ac8c3f03309d5bef80
Abhilash107 <abhilash00003108884@gmail.com>
2024-09-15 21:41:48 +0530

    project setup

---
+++ themeSwitcher/src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { ThemeProvider } from './contexts/theme'
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light")
+  //to add functionality to the pros/val;ies in ThemeProvider,
+  //define function's name same as the props name
+
+  const lightTheme = ()=>{
+    setThemeMode("light")
+  }
+
+  const darkTheme = ()=>{
+    setThemeMode("dark")
+  }
+
+  //actual change in themes
+  useEffect(  ()=>{
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  return (
+
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/*Btn */}
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                      {/*Card */}
+                    </div>
+                </div>
+        </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
