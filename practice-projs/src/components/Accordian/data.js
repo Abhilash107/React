b39dc68e181bded3449bed08cb928de5de72891d
Abhilash107 <abhilash00003108884@gmail.com>
2024-09-17 21:42:51 +0530

    project set up

---
+++ practice-projs/src/components/Accordian/data.js
@@ -0,0 +1,23 @@
+const dataArray = [
+    {
+      id: 1,
+      qns: "What is React?",
+      ans: "React is a JavaScript library for building user interfaces."
+    },
+    {
+      id: 2,
+      qns: "What is a component in React?",
+      ans: "A component is a reusable piece of code that can be used to build the UI."
+    },
+    {
+      id: 3,
+      qns: "What is state in React?",
+      ans: "State is a built-in object that stores property values that belong to a component."
+    },
+    {
+      id: 4,
+      qns: "What is a prop in React?",
+      ans: "Props (short for properties) are read-only attributes used to pass data from one component to another."
+    }
+  ];
+  
