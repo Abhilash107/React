0c0a0daac1deb7fe1804b347fb9517298a257820
Abhilash107 <abhilash00003108884@gmail.com>
2024-10-28 09:03:22 +0530

    projects

--- Projects/src/App.jsx
+++ Projects/src/App.jsx
@@ -9,6 +9,8 @@ import ImageSlider from './components/ImageSlider/ImageSlider'
 import NavigationMenu from './components/NavigationMenu/NavigationMenu'
 import menus from './components/NavigationMenu/data'
 import QRCodeGenerator from './components/QRCode/QrCode'
+import ThemeToggle from './components/ThemeToggle/ThemeToggle'
+import Scroll from './components/ScrollIndicator/Scroll'
 
 function App() {
 
@@ -21,7 +23,10 @@ function App() {
       {/* <StarRating noOfStars={5}/> */}
       {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10}/> */}
       {/* <NavigationMenu menus={menus}/> */}
-      <QRCodeGenerator/>
+      {/* <QRCodeGenerator/> */}
+      {/* <ThemeToggle/> */}
+      {/* <Scroll url={'https://dummyjson.com/products?limit=100'}/> */}
+      
     </div>
 
   )
